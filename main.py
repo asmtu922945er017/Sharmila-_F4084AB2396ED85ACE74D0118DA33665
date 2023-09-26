90aa10e883e59c8b6e4c9a2a18939f9903c1f172
asmtu922945er017 <145832176+asmtu922945er017@users.noreply.github.com>
2023-09-26 10:13:34 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-num = int(input("Enter a number: "))    
-factorial = 1    
-if num < 0:    
-   print(" Factorial does not exist for negative numbers")    
-elif num == 0:    
-   print("The factorial of 0 is 1")    
-else:    
-   for i in range(1,num + 1):    
-       factorial = factorial*i    
-   print("The factorial of",num,"is",factorial)    
+def CheckLeap(Year):  
+  # Checking if the given year is leap year  
+  if((Year % 400 == 0) or  
+     (Year % 100 != 0) and  
+     (Year % 4 == 0)):   
+    print("Given Year is a leap Year");  
+  # Else it is not a leap year  
+  else:  
+    print ("Given Year is not a leap Year")  
+# Taking an input year from user  
+Year = int(input("Enter the number: "))  
+# Printing result  
+CheckLeap(Year)  
